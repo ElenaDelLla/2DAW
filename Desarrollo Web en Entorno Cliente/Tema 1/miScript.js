5d0278d7628a11d7f5b3834678536ada9b56cb4b
Elena Delgado Llamas <elena@MacBook-Pro-de-Elena.local>
2022-09-21 11:36:17 +0200

    ejercicios tema1 JavaScript

---
+++ Desarrollo Web en Entorno Cliente/Tema 1/miScript.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+const boton = document.getElementById("saludo");
+
+boton.addEventListener ("click", function saludar() {
+   // alert("Hola Mundo!!!");
+});
+}
---
+++ Desarrollo Web en Entorno Cliente/Tema 1/miScriptDesofuscado.js
@@ -0,0 +1,6 @@
+window['onload'] = function() {
+    const _0xe90ax1 = document['getElementById']('saludo');
+    _0xe90ax1['addEventListener']('click', function _0xe90ax2() {
+        alert('Hola Mundo!!!')
+    })
+}
